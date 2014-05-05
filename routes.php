cbf315af8ddbe9121028a2b93d792066231951b3
zairl23 <756127792@qq.com>
2014-05-05 22:09:40 +0800

    add

--- routes.php
+++ routes.php
@@ -3,12 +3,8 @@
 
   		Route::get('/', 'Controller@index');
 
-  		Route::resource('videos', 'Controller');
+  		Route::resource('list', 'Controller');
 
-  		Route::resource('tags', 'TagsController');
-
-  		Route::resource('topics', 'TopicsController');
-
-  		Route::get('data/index', array('as' => 'data.index', 'uses' => 'HomeController@data'));
+        Route::get('data/index', array('as' => 'data.index', 'uses' => 'HomeController@data'));
 	});
 ?>
